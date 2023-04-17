45baa875f6ad1bf531d7eddf3d86fe30056f691d
jason22432150 <52876522+jason22432150@users.noreply.github.com>
2023-04-17 14:37:42 +0800

    Finish

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pyautogui
 
-pyautogui.write("Python is an amazing programming language.")
+pyautogui.keyDown("alt")
+pyautogui.keyDown("tab")
+
+pyautogui.keyUp("alt")
+pyautogui.keyUp("tab")
+
+pyautogui.write("https://drive.google.com/file/d/1IaZ_T9yCgsDEdKljTXZ_eFKR3Gl7-mxA/view?usp=sharing")
