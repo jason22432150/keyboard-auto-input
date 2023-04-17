3c31a0be8895e34d1eaf6244d6ff9ba55cbc663b
jason22432150 <52876522+jason22432150@users.noreply.github.com>
2023-04-17 15:16:08 +0800

    update

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ pyautogui.keyDown("tab")
 pyautogui.keyUp("alt")
 pyautogui.keyUp("tab")
 
-pyautogui.write("https://drive.google.com/file/d/1IaZ_T9yCgsDEdKljTXZ_eFKR3Gl7-mxA/view?usp=sharing")
+str = "./nikto.pl -h http://www.example.com"
+pyautogui.write(str)
+
+pyautogui.keyDown("enter")
+pyautogui.keyUp("enter")
